79415caf26dd1f3d8da1d6d543bb321300a20cab
Ch4s3r <lamprecht.patrick1@gmail.com>
2024-05-15 16:41:52 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/bootbuildimagearm/BootbuildimageArmApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bootbuildimagearm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootbuildimageArmApplication
+
+fun main(args: Array<String>) {
+    runApplication<BootbuildimageArmApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bootbuildimagearm/BootbuildimageArmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bootbuildimagearm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootbuildimageArmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
